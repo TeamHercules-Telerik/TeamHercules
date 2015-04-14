b187f4385105087db1a2f8b7442d5993c0c530bb
marindraganov <marodraganov@gmail.com>
2015-04-14 18:00:51 +0300

    SVG Drawer is defined as module

--- scripts/SVG_Drawer.js
+++ scripts/SVG_Drawer.js
@@ -1,48 +1,61 @@
-function drawField(fieldWalls, cellHeight, wallHeight) {
+define(function () {
+    'use strict'
+    var SVG_Drawer = (function () {
+        function SVG_Drawer() {
+        };
 
-    var svgNS = 'http://www.w3.org/2000/svg';
+        SVG_Drawer.prototype.DrawField = function (fieldWalls, cellHeight, wallHeight) {
 
-    for (var i = 0; i < fieldWalls.length; i++) {
-        var y = cellHeight * (~~(i / 2));
-        for (var j = 0; j < fieldWalls[i].length; j++) {
+            var svgNS = 'http://www.w3.org/2000/svg';
 
-            var rect = document.createElementNS(svgNS, 'rect');
+            for (var i = 0; i < fieldWalls.length; i++) {
+                var y = cellHeight * (~~(i / 2));
+                for (var j = 0; j < fieldWalls[i].length; j++) {
 
-            if (fieldWalls[i][j] === "-") {
-                rect.setAttribute('x', cellHeight * j);
-                rect.setAttribute('y', y);
-                rect.setAttribute('width', cellHeight + wallHeight);
-                rect.setAttribute('height', wallHeight);
-                rect.setAttribute('class', 'wall');
-                document.getElementById('game').appendChild(rect);
+                    var rect = document.createElementNS(svgNS, 'rect');
+
+                    if (fieldWalls[i][j] === "-") {
+                        rect.setAttribute('x', cellHeight * j);
+                        rect.setAttribute('y', y);
+                        rect.setAttribute('width', cellHeight + wallHeight);
+                        rect.setAttribute('height', wallHeight);
+                        rect.setAttribute('class', 'wall');
+                        document.getElementById('game').appendChild(rect);
+                    }
+                    else if (fieldWalls[i][j] === "|") {
+                        rect.setAttribute('x', cellHeight * j);
+                        rect.setAttribute('y', y);
+                        rect.setAttribute('width', wallHeight);
+                        rect.setAttribute('height', cellHeight + wallHeight);
+                        rect.class = 'wall';
+                        document.getElementById('game').appendChild(rect);
+                    }
+                }
             }
-            else if (fieldWalls[i][j] === "|") {
-                rect.setAttribute('x', cellHeight * j);
-                rect.setAttribute('y', y);
-                rect.setAttribute('width', wallHeight);
-                rect.setAttribute('height', cellHeight + wallHeight);
-                rect.class = 'wall';
-                document.getElementById('game').appendChild(rect);
-            }
+
+            //TODO union
+            var signUp = document.createElementNS(svgNS, 'text');
+            signUp.setAttribute('x', 412);
+            signUp.setAttribute('y', 190);
+            signUp.innerHTML = 'JavaScript';
+            document.getElementById('game').appendChild(signUp);
+
+            var signDown = document.createElementNS(svgNS, 'text');
+            signDown.setAttribute('x', 430);
+            signDown.setAttribute('y', 220);
+            signDown.innerHTML = 'Museum';
+            document.getElementById('game').appendChild(signDown);
+
+            var levelSignUp = document.createElementNS(svgNS, 'text');
+            levelSignUp.setAttribute('x', 18);
+            levelSignUp.setAttribute('y', 85);
+            levelSignUp.innerHTML = 'level';
+            document.getElementById('game').appendChild(levelSignUp);
+
         }
-    }
 
-    //TODO union
-    var signUp = document.createElementNS(svgNS, 'text');
-    signUp.setAttribute('x', 412);
-    signUp.setAttribute('y', 190);
-    signUp.innerHTML = 'JavaScript';
-    document.getElementById('game').appendChild(signUp);
+        return SVG_Drawer;
+    })();
 
-    var signDown = document.createElementNS(svgNS, 'text');
-    signDown.setAttribute('x', 430);
-    signDown.setAttribute('y', 220);
-    signDown.innerHTML = 'Museum';
-    document.getElementById('game').appendChild(signDown);
-
-    var levelSignUp = document.createElementNS(svgNS, 'text');
-    levelSignUp.setAttribute('x', 18);
-    levelSignUp.setAttribute('y', 85);
-    levelSignUp.innerHTML = 'level';
-    document.getElementById('game').appendChild(levelSignUp);
-}
+    return new SVG_Drawer();
+});
